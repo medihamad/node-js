7b546a8b0bcd1abc47055397eb45871d8217517f
Medi Hamad <kumarpatel692@gmail.com>
2021-05-08 16:42:44 +0300

    responding with html pages

--- index.js
+++ index.js
@@ -3,4 +3,19 @@
 const http = require('http');
 const fs = require('fs');
 
-const homepage = fs.readFileSync('index.html');
+const homePage = fs.readFileSync('index.html');
+const about = fs.readFileSync('about.html');
+const contact = fs.readFileSync('contact.html');
+const noPageFound = fs.readFileSync('notfound.html');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/about') res.end(about);
+    else if (req.url === '/contact') res.end(contact);
+    else if (req.url === '/') res.end(homePage);
+    else {
+        res.writeHead(404);
+        res.end(noPageFound);
+    }
+});
+
+server.listen(3000);
