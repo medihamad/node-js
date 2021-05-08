eba3b57cd2457ae1e8434caea0022d85025fb35b
Medi Hamad <kumarpatel692@gmail.com>
2021-05-08 14:49:20 +0300

    responding with different messages depending on the request

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-//creating first server in node
+//responding with different messages depending on the request
 
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end('Welcome to Node js');
-})
+    if(req.url === '/') res.end('HomePage');
+    else if (req.url === '/contact') res.end('contact page');
+    else if (req.url === '/about') res.end('about page');
+    else {
+        res.writeHead(404);
+        res.end('page not found');
+    }
+});
 
 server.listen(3000);
